dc80d3a4a389976700fd022641f5a2fc3ae37ee9
pikkauhn <114180449+pikkauhn@users.noreply.github.com>
2024-03-25 11:04:03 -0500

    made toast separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,10 @@
-'use client'
-import { useEffect, useRef } from "react";
-import { Toast } from 'primereact/toast';
-import { MessageSeverity } from "primereact/api";
-import SessionInfo from "./components/SessionInfo";
+import ServerSpin from "./components/ServerSpin";
 
 export default function Home() {
-  const toastTopCenter = useRef<Toast>(null);
-
-  useEffect(() => {
-    let ignore = false
-    const loadData = async () => {
-      try {
-        // Notify the user that the server is spinning up
-        showMessage(MessageSeverity.WARN, 'Server Spinning Up', 'Please wait...');
-        const session = await SessionInfo();
-
-        // Update the message once the server is up
-        showMessage(MessageSeverity.SUCCESS, 'Server is Up', 'Data loaded successfully.');
-      } catch (error) {
-        console.log(error);
-        // Notify the user about the server error
-        showMessage(MessageSeverity.ERROR, 'Server Error', 'An error occurred.');
-      }
-    };
-
-    loadData();
-  }, []);
-
-  const showMessage = (severity: MessageSeverity, summary: string | null, detail: string | null) => {
-    toastTopCenter.current?.show([
-      { severity: severity, summary: summary, detail: detail, life: 3000 }
-    ])
-  }
 
   return (
     <div className=''>
+      <ServerSpin />      
     </div>
   )
 }
